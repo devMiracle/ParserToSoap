337bb1eaf914bdd3513cdc3289c67ecee5b8b029
Miracle <rjcvjchzljv9@gmail.com>
2020-01-16 02:06:43 +0200

    Initial commit

---
+++ ParserToSoap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+
+namespace ParserToSoap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Oriana");
+            string path = "test.soap";
+            SoapFormatter soap = new SoapFormatter();
+
+            //write
+            //using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            //{
+            //    soap.Serialize(fs, person);
+            //    Console.WriteLine("Объект сериализован");
+            //}
+
+            //read
+            //using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            //{
+            //    Person person1 = (Person)soap.Deserialize(fs);
+            //    Console.WriteLine("Объект десериализован");
+            //    Console.WriteLine(person1.Name);
+            //}
+
+            Console.ReadKey(true);
+        }
+    }
+}
